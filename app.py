90f74130e906d3171d0ebbc1146e6ecf93f9a53d
ruble0m46 <138867878+ruble0m46@users.noreply.github.com>
2025-07-12 14:05:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,6 @@ def get_weather():
     return jsonify(res.json())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
