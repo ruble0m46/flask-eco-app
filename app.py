0e92b72a96fe631c4e92a2347cf4eecbcde4595f
ruble0m46 <138867878+ruble0m46@users.noreply.github.com>
2025-07-12 13:59:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-# 이 코드는 app.py 안에 붙여넣기
 from flask import Flask, request, jsonify, render_template
 import requests
 
 app = Flask(__name__)
-API_KEY = 'f8af007bb8d199bd6612f11a4fc70e6b' 
+API_KEY = 'f8af007bb8d199bd6612f11a4fc70e6b'  # 너의 OpenWeatherMap API 키
 
-@app.route('')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('get_weather', methods=['POST'])
+@app.route('/get_weather', methods=['POST'])
 def get_weather():
     data = request.get_json()
     lat = data['lat']
     lon = data['lon']
-    url = f'httpsapi.openweathermap.orgdata2.5weatherlat={lat}&lon={lon}&appid={API_KEY}&units=metric&lang=kr'
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric&lang=kr'
     res = requests.get(url)
     return jsonify(res.json())
 
